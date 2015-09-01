4b45f01f2500ea660eb50fcde99ab2ef82f6fa75
Craig <craigmarroquin@yahoo.com>
2015-08-31 23:40:29 -0700

    Not much in the file but just starting us off.

---
+++ Driver.cpp
@@ -0,0 +1,14 @@
+/*****************************************************************************************************
+* Title: Driver.cpp
+* Class: CPSC 362 - Software Testing
+* Group: Sea + Plus
+* Description: Main driver program.
+******************************************************************************************************/
+#include <iostream> 
+
+using namespace std;
+
+int main(){
+	
+	return 0;
+}
